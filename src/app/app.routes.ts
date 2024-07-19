e121ffbc70e6c0d3264bbfcebb83447c60e525ca
trilok <trilok@gaiasys.in>
2024-07-19 14:35:17 +0530

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { TutorialsComponent } from './tutorials/tutorials.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent, title: 'App Home Page'},
-    {path: 'user', component: UserComponent, title: 'App User Page'}
-
+    {path: 'user', component: UserComponent, title: 'App User Page'},
+    {path: 'tutorials', component: TutorialsComponent, title: 'App Tutorials Page'}
 ];
