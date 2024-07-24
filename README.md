7720cdabd8696bbfad67f69dd9f7260d6cae9837
trilok <trilok@gaiasys.in>
2024-07-24 22:36:42 +0530

    local storage with angular

--- README.md
+++ README.md
@@ -25,3 +25,13 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+# The Local Storage API
+
+> The localStorage object helps us store data, and this API provides four methods to help us interact with the data stored in the browser
+
+* setItem
+* getItem
+* removeItem
+* clear
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,32 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-17';
+  private storageKey = 'user-background';
+  private defaultColor = '#ff00ff';
+  userBackgroundColor = null;
+
+  ngOnInit(): void {
+    this.loadBackgroundPreferences();
+  }
+
+  private getBackgroundColor(): string | any {
+    return localStorage.getItem(this.storageKey) || this.defaultColor;
+  }
+  public saveBackgroundColor(color: string): void {
+    localStorage.setItem(this.storageKey, color);
+    this.loadBackgroundPreferences();
+  }
+  public removePreferences(): void {
+    localStorage.removeItem(this.storageKey);
+    this.loadBackgroundPreferences();
+  }
+
+  private loadBackgroundPreferences(): void {
+    this.userBackgroundColor = this.getBackgroundColor();
+    window.document.body.style.backgroundColor = this.getBackgroundColor();
+  }
 }
