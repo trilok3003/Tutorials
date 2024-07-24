4e6618bf049c783aecbca0eeae52cbbd5f83b83b
trilok <trilok@gaiasys.in>
2024-07-24 22:44:47 +0530

    angular-selected-dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-17';
+  selectedTeam = '';
+  selectedTeam2 = '';
+  selectedTeam3 = '';
+
+
+  @ViewChild('teams2') teams!: ElementRef;
+  onSelected(value: string): void {
+    this.selectedTeam = value;
+  }
+  onSelected2():void {
+		this.selectedTeam2 = this.teams.nativeElement.value;
+	}
 }
